c9821cc07dd030f8721951140c41f011bedcd3d9
Olivier Roy <120348668+KingReiviloYor@users.noreply.github.com>
2023-03-27 16:34:50 -0400

    Add files via upload

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,20 +6,34 @@
 /*   By: oroy <oroy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 16:29:41 by oroy              #+#    #+#             */
-/*   Updated: 2023/03/23 19:12:46 by oroy             ###   ########.fr       */
+/*   Updated: 2023/03/27 16:34:20 by oroy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include "get_next_line.h"
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <stdlib.h>
 # include <limits.h>
 
-typedef	struct s_buf
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+typedef struct s_buf
 {
 	char	buf[BUFFER_SIZE + 1];
 	size_t	i;
-} 	t_buf;
+}	t_buf;
+
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin_gnl(char *newline, char *line, char *buf, size_t buf_len);
+size_t	ft_strlen(const char *s);
+size_t	getbuflength(char *buf, unsigned char *n);
+char	*get_next_line(int fd);
 
 #endif
